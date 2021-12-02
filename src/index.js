95817acb33f0d57910d210d803c4fb94da064d59
Vitaliy <270vitalik270@gmail.com>
2021-12-02 14:32:48 +0200

    added connection to firebase

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import firebase from 'firebase'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Initialize Firebase
+initializeApp({
+  apiKey: "AIzaSyDX4NSHEEZEFot0XG2oSln9hccrz8Q9U9k",
+  authDomain: "realtime-chart-ulbi.firebaseapp.com",
+  projectId: "realtime-chart-ulbi",
+  storageBucket: "realtime-chart-ulbi.appspot.com",
+  messagingSenderId: "301686107390",
+  appId: "1:301686107390:web:285693b158fab0e86349af",
+  measurementId: "G-1TTD1QQ22M"
+});
+const Context = createContext(null)
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Context.Provider value={{
+      auth,
+      firebase,
+      firestore
+    }}>
+      <App />
+    </Context.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
